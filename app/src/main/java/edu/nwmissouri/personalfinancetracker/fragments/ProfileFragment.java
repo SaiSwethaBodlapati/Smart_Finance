d08e1c3b2c593e187dcf605de87111cb94ee7702
SaiSwethaBodlapati <saiswethabodlapati@gmail.com>
2024-10-11 22:45:01 -0500

    DoneActivitychartandSplashscreen

---
+++ app/src/main/java/edu/nwmissouri/personalfinancetracker/fragments/ProfileFragment.java
@@ -0,0 +1,40 @@
+package edu.nwmissouri.personalfinancetracker.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+import edu.nwmissouri.personalfinancetracker.Manager.DatabaseManager;
+import edu.nwmissouri.personalfinancetracker.R;
+
+public class ProfileFragment extends Fragment {
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        // Initialize your database class
+
+        // Find the TextView elements in your layout
+        TextView firstNameTextView = rootView.findViewById(R.id.textFirstName);
+        TextView lastNameTextView = rootView.findViewById(R.id.textLastName);
+        TextView emailTextView = rootView.findViewById(R.id.textEmail);
+        TextView mobileTextView = rootView.findViewById(R.id.textMobile);
+
+        // Set the text for each TextView with data from your database
+        firstNameTextView.setText(DatabaseManager.getInstance().getFirstName());
+        lastNameTextView.setText(DatabaseManager.getInstance().getLastName());
+        emailTextView.setText(DatabaseManager.getInstance().getEmail());
+        mobileTextView.setText(DatabaseManager.getInstance().getMobile());
+
+        return rootView;
+    }
+}
